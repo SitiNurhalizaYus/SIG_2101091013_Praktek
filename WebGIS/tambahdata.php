5fe5a4b7ef3db6c03304b0004e628b1e00408285
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2022-12-12 12:13:01 +0700

    Update tambahdata.php

--- WebGIS/tambahdata.php
+++ WebGIS/tambahdata.php
@@ -7,8 +7,12 @@ $details = strip_tags($_POST['details']);
 $latitude = strip_tags($_POST['latitude']);
 $longitude = strip_tags($_POST['longitude']);
 $telephone = strip_tags($_POST['telephone']);
+// $name = strip_tags($_POST['name']);
+// $geolocation = strip_tags($_POST['geolocation']);
 
 $conn->addCompany($company, $details, $latitude, $longitude, $telephone);
+$conn->addStreets($name, $geolocation);
+//$conn->addAreas($name, $geolocation);
 ?>
 <!DOCTYPE html>
 <html>
