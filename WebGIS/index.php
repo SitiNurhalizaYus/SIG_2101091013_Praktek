cb63617262f187c4d57677e14ce80e8011ef415a
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2023-01-03 10:37:39 +0700

    Update index.php

--- WebGIS/index.php
+++ WebGIS/index.php
@@ -9,31 +9,81 @@
 <html>
 <head>
  <title>Leaflet basic example</title>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" />
-  <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" />
+  <!-- <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"></script>  -->
+  <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+  <style>
+		html, body {
+			height: 100%;
+			margin: 0;
+		}
+		.leaflet-container {
+			height: 400px;
+			width: 600px;
+			max-width: 100%;
+			max-height: 100%;
+		}
+	</style>
 </head>
 <body>
- <div id="map" style="width: 600px; height: 400px"></div>
+ <div id="map" style="width: 1350px; height: 500px"></div>
  <script>
 
-const map = L.map('map').setView([-0.9137429958385986, 100.3863016659671], 5);
+//const map = L.map('map').setView([-0.9137429958385986, 100.3863016659671], 5);
 
-const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   maxZoom: 19,
+//   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
 
-  
+var perusahaan = L.layerGroup();
+var jalan = L.layerGroup();
+var area = L.layerGroup();
+
+  const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	});
+
+  var mapboxUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+  var streets = L.tileLayer(mapboxUrl, {
+        id : 'mapbox/streets-v11',
+        tileSize : 512,
+        zoomOffset : -1
+  });
+  var satellite = L.tileLayer(mapboxUrl, {
+        id : 'mapbox/satellite-v9',
+        tileSize : 512,
+        zoomOffset : -1
+  });
+  var map = L.map('map',{
+    center : [-0.9168991900273032, 100.4535596259778],
+    zoom : 5,
+    layers : [osm,perusahaan]
+  });
+
   $( document ).ready(function() {
    addCompanies();   
    addStreets();   
    addAreas();   
   });
+
+  var baseMaps = {
+    "OpenStreetMap" : osm,
+    "MapBoxStreet" : streets,
+    "Satellite" : satellite
+  };
+
+  var overlays = {
+    "Perusahaan" : perusahaan,
+    "Jalan" : jalan,
+    "Area" : area
+  };
   
   function addCompanies() {
    for(var i=0; i<companies.length; i++) {
-    var marker = L.marker( [companies[i]['latitude'], companies[i]['longitude']]).addTo(map);
+    var marker = L.marker( [companies[i]['latitude'], companies[i]['longitude']]).addTo(perusahaan);
     marker.bindPopup( "<b>" + companies[i]['company']+"</b><br>Details:" + companies[i]['details'] + "<br />Telephone: " + companies[i]['telephone']);
    }
   }
@@ -60,24 +110,35 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
    
    return latLngLine;
   }
-  
+
   function addAreas() {
    for(var i=0; i < areas.length; i++) {
-    var polygon = L.polygon( stringToGeoPoints(areas[i]['geolocations']), { color: 'blue'}).addTo(map);
-    polygon.bindPopup( "<b>" + areas[i]['name']);   
+    var polygon = L.polygon( stringToGeoPoints(areas[i]['geolocations_areas']), { color: 'blue'}).addTo(area);
+    polygon.bindPopup( "<b>" + areas[i]['name_areas']);   
    }
   }
   
   function addStreets() {
    for(var i=0; i < streets.length; i++) {
-    var polyline = L.polyline( stringToGeoPoints(streets[i]['geolocations']), { color: 'red'}).addTo(map);
-    polyline.bindPopup( "<b>" + streets[i]['name']);   
+    var polyline = L.polyline( stringToGeoPoints(streets[i]['geolocations_streets']), { color: 'red'}).addTo(jalan);
+    polyline.bindPopup( "<b>" + streets[i]['name_streets']);   
    }
   }
   
   var companies = JSON.parse( '<?php echo json_encode($companies) ?>' );
   var streets = JSON.parse( '<?php echo json_encode($streets) ?>' );
   var areas = JSON.parse( '<?php echo json_encode($areas) ?>' );
+  var layerControl = L.control.layers(baseMaps, overlays).addTo(map);
  </script>
-</body>
+
+<h1>Tambah Data</h1>
+   <a href="inputmarker.php">1.input marker</a><br>
+   <a href="inputpolyline.php">2.input polyline(streets)</a><br>
+   <a href="inputpolygon.php">3.input polygon(areas)</a>
+  
+
+  </body>
+
 </html>
+
+
