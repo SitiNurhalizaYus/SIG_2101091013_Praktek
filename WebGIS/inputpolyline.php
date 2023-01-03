c41b291fc788c62a934018325f8b62031ba94fc8
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2023-01-03 10:37:31 +0700

    Create inputpolyline.php

---
+++ WebGIS/inputpolyline.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+
+<h1>Tambah Data</h1>
+   <h3>Polyline</h3>
+    <form action="tambahdata2.php" method="post" enctype="multipart/form-data">
+        
+    <table>
+            <tr>
+                <td>Name</td>
+                <td>:</td>                
+                <td><input type="text" name="name_streets"></td>
+            </tr>
+            <tr>
+                <td>Geolocations</td>
+                <td>:</td>                
+                <td><input type="text" name="geolocations_streets"></td>     
+            </tr>
+            <tr>
+                <td></td>
+                <td></td> 
+                <td colspan=3><input type="submit" name="submit" value="Kirim"></td>
+            </tr>
+        </table>
+    </form>    
+</body>
+</html>
