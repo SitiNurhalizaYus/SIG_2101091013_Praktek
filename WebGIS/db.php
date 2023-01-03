08b819558e619c9b664c9a4ad5db93cc5efcf13e
SitiNurhalizaYus <114122090+SitiNurhalizaYus@users.noreply.github.com>
2023-01-03 10:37:06 +0700

    Update db.php

--- WebGIS/db.php
+++ WebGIS/db.php
@@ -14,59 +14,66 @@ function __destruct()
   $this->conn->close();
 }
 
-public function addCompany( $company, $details, $latitude, $longitude, $telephone){
-$statement = $this->conn->prepare("Insert INTO companies( company, details, latitude, longitude, telephone) VALUES(?, ?, ?, ?, ?)");
+public function addCompanies( $company, $details, $latitude, $longitude, $telephone){
+    $statement = $this->conn->prepare("Insert INTO companies( company, details, latitude, longitude, telephone) VALUES(?, ?, ?, ?, ?)");
+
 $statement->bind_param('sssss', $company, $details, $latitude, $longitude, $telephone);
+
 $statement->execute();
+
 $statement->close();
 }
 
-public function addStreats( $name, $geolocation){
-$statement = $this->conn->prepare("Insert INTO streets( name, geolocation) VALUES(?, ?)");
-$statement->bind_param('sssss', $name, $geolocation);
+public function addStreets( $name_streets, $geolocations_streets){
+  $statement = $this->conn->prepare("Insert INTO streets(name_streets, geolocations_streets) VALUES(?, ?)");
+
+$statement->bind_param('ss', $name_streets, $geolocations_streets);
+
 $statement->execute();
+
 $statement->close();
 }
 
-public function addAreas($name, $geolocation){
-$statement = $this->conn->prepare("Insert INTO areas( name, geolocation) VALUES(?, ?)");
-$statement->bind_param('sssss', $name, $geolocation);
+public function addAreas( $name_areas, $geolocations_areas){
+  $statement = $this->conn->prepare("Insert INTO areas(name_areas, geolocations_areas) VALUES(?, ?)");
+
+$statement->bind_param('ss', $name_areas, $geolocations_areas);
+
 $statement->execute();
+
 $statement->close();
 }
 
 
 public function getCompaniesList()
 {
-  $query = "SELECT * FROM companies";
-  $result = mysqli_query($this->conn, $query);
-  $rows = [];
-  while ($row = mysqli_fetch_assoc($result)) {
-     $rows[] = $row;
-     
-  }
-  return $rows;
+    $query = "SELECT * FROM companies";
+    $data = mysqli_query($this->conn, $query);
+    while ($row = mysqli_fetch_array($data)){
+        $hasil[] = $row;
+    }
+    return $hasil;
 }
+ 
 public function getStreetsList()
 {
-  $query = "SELECT * FROM streets ";
-  $result = mysqli_query($this->conn, $query);
-  $rows = [];
-  while ($row = mysqli_fetch_assoc($result)) {
-     $rows[] = $row;
-     
-  }
-  return $rows;
+    $query = "SELECT * FROM streets";
+    $data = mysqli_query($this->conn, $query);
+    while ($row = mysqli_fetch_array($data)){
+        $hasil[] = $row;
+    }
+    return $hasil;
 }
+
 public function getAreasList()
 {
-  $query = "SELECT * FROM areas ";
-  $result = mysqli_query($this->conn, $query);
-  $rows = [];
-  while ($row = mysqli_fetch_assoc($result)) {
-     $rows[] = $row;
-     
-  }
-  return $rows;
+    $query = "SELECT * FROM areas";
+    $data = mysqli_query($this->conn, $query);
+    while ($row = mysqli_fetch_array($data)){
+        $hasil[] = $row;
+    }
+    return $hasil;
 }
+
 }
+?>
